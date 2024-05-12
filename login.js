40967ed5a9ad66275ab5fac0f0e6fce06123b5a7
Xais69 <164960280+Xais69@users.noreply.github.com>
2024-05-11 21:34:54 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,23 @@
+const url = "https://go-wash-api.onrender.com/api/login"
+async function login(){
+
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('senha').value;
+
+    let response = await fetch(url, {
+        method:"POST",
+        body:JSON.stringify({
+            "email": email,
+            "password":password,
+            "user_type_id":1
+
+        }),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    });
+
+    let responseApi = await response.json();
+
+    console.log(responseApi);
+}
