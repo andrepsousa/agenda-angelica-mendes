5aed344113abe25900cac7cf3808411dc0b1b4b1
Andre Pereira <andre.pds07@gmail.com>
2024-05-14 21:40:37 -0300

    Adicionando listagem de erros retornados pela API

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,6 +1,6 @@
 const url = 'https://go-wash-api.onrender.com/api/user';
 
-async function cadastroUsuario(){   
+async function cadastroUsuario() {
 
     var name = document.getElementById('fullname').value;
     var email = document.getElementById('email').value;
@@ -8,95 +8,95 @@ async function cadastroUsuario(){
     var password = document.getElementById('password').value;
     var date = document.getElementById('date').value;
 
-    if(name ==='' || email === '' || cpf_cnpj === '' || password === '' || date === ''){
+    if (name === '' || email === '' || cpf_cnpj === '' || password === '' || date === '') {
         alert('Preencha todos os campos');
         return;
     }
 
-    if (cpf_cnpj.length < 11) { 
+    if (cpf_cnpj.length < 11) {
         alert('CPF incompleto. Por favor, digite os 11 dígitos do CPF.');
         return;
     }
 
-    if (!validarCPF(cpf_cnpj)) { 
+    if (!validarCPF(cpf_cnpj)) {
         alert('CPF inválido.');
         return;
     }
 
-function validarCPF(cpf) {
-    cpf = cpf.replace(/[^\d]/g, '');
-    
-    var soma = 0;
-    for (var i = 0; i < 9; i++) {
-        soma += parseInt(cpf.charAt(i)) * (10 - i);
-    }
-    var resto = soma % 11;
-    var digitoVerificador1 = resto < 2 ? 0 : 11 - resto;
-    
-    if (parseInt(cpf.charAt(9)) !== digitoVerificador1) {
-        return false;
-    }
-    
-    soma = 0;
-    for (var i = 0; i < 10; i++) {
-        soma += parseInt(cpf.charAt(i)) * (11 - i);
-    }
-    resto = soma % 11;
-    var digitoVerificador2 = resto < 2 ? 0 : 11 - resto;
-    
-    if (parseInt(cpf.charAt(10)) !== digitoVerificador2) {
-        return false;
-    }
-    
-    return true;
-}
+    function validarCPF(cpf) {
+        cpf = cpf.replace(/[^\d]/g, '');
 
-    if (!validarEmail(email)) { 
+        var soma = 0;
+        for (var i = 0; i < 9; i++) {
+            soma += parseInt(cpf.charAt(i)) * (10 - i);
+        }
+        var resto = soma % 11;
+        var digitoVerificador1 = resto < 2 ? 0 : 11 - resto;
+
+        if (parseInt(cpf.charAt(9)) !== digitoVerificador1) {
+            return false;
+        }
+
+        soma = 0;
+        for (var i = 0; i < 10; i++) {
+            soma += parseInt(cpf.charAt(i)) * (11 - i);
+        }
+        resto = soma % 11;
+        var digitoVerificador2 = resto < 2 ? 0 : 11 - resto;
+
+        if (parseInt(cpf.charAt(10)) !== digitoVerificador2) {
+            return false;
+        }
+
+        return true;
+    }
+
+    if (!validarEmail(email)) {
         alert('E-mail inválido.');
         return;
     }
 
-function validarEmail(email) {
-    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-}
+    function validarEmail(email) {
+        var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(email);
+    }
 
-    let resposta = await fetch(url,{
-        method:"POST",
+    let resposta = await fetch(url, {
+        method: "POST",
         body: JSON.stringify(
             {
-                "name":name,
-                "email":email,
-                "user_type_id":1,
+                "name": name,
+                "email": email,
+                "user_type_id": 1,
                 "password": password,
                 "cpf_cnpj": cpf_cnpj,
                 "terms": 1,
                 "birthday": date
             }
         ),
-        headers:{
+        headers: {
             'Content-Type': 'application/json'
-        }        
+        }
     });
-    
+
     let respostaApi = await resposta.json();
 
-    if(respostaApi.data.statusCode == 422){
-        if(respostaApi.data.errors.cpf_cnpj){
+    if (respostaApi.data.statusCode == 422) {
+        if (respostaApi.data.errors.cpf_cnpj) {
             alert('CPF já cadastrado. Por favor, insira um CPF válido.');
         }
-        if(respostaApi.data.errors.email){
+        if (respostaApi.data.errors.email) {
             alert('Este E-mail já possui cadastro.')
             return;
         }
-        if(respostaApi.data.errors.password){
+        if (respostaApi.data.errors.password) {
             alert('A senha deve conter pelo menos 6 caracteres.')
             return;
         }
-        if(respostaApi.data.errors.name){
+        if (respostaApi.data.errors.name) {
             alert(respostaApi.data.errors.name[0]);
         }
-        if(respostaApi.data.errors.date){
+        if (respostaApi.data.errors.date) {
             alert(respostaApi.data.errors.date[0]);
         }
         return;
--- script/login.js
+++ script/login.js
@@ -1,25 +1,41 @@
-const url = "https://go-wash-api.onrender.com/api/login"
-async function login(){
+const url = "https://go-wash-api.onrender.com/api/login";
 
+async function login() {
     let email = document.getElementById('email').value;
     let password = document.getElementById('senha').value;
 
     let response = await fetch(url, {
-        method:"POST",
-        body:JSON.stringify({
+        method: "POST",
+        body: JSON.stringify({
             "email": email,
-            "password":password,
-            "user_type_id":1
-
+            "password": password,
+            "user_type_id": 1
         }),
-        headers:{
-            'Content-Type': 'application/json',
+        headers: {
+            'Content-Type': 'application/json'
         }
     });
 
-    let responseApi = await response.json();
+    if (response.status === 401) { 
+        alert("Credenciais inválidas. Por favor, verifique seu e-mail e senha.");
+        return;
+    }
 
-    localStorage.setItem('user', JSON.stringify(responseApi))
-    
-    window.location.href="home.html"
+    if (response.status === 404) {
+        alert("Usuário não encontrado. Por favor, verifique seu e-mail.");
+        return;
+    }
+
+    if (response.status === 422) {
+        let responseData = await response.json();
+        alert(responseData.message);
+        return;
+    }
+
+    if (response.ok) { 
+        let responseApi = await response.json();
+        localStorage.setItem('user', JSON.stringify(responseApi));
+        console.log(responseApi);
+        window.location.href = "home.html";
+    }
 }
